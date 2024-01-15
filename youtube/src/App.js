70013aff324edaf55c1557c7b32d724e2fe1eebe
Samanvith <Samanvith2005@gmail.com>
2024-01-15 14:46:45 +0530

    Added tailwind.css

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>
+    <div className='font-bold bg-red-500'>
         <h1>Youtube clone</h1>
     </div>
   )
 }
-
 export default App
